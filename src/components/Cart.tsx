8270179182b8ad298223b716fa88e9186343b379
Vinicius <viniholtman28@gmail.com>
2022-05-27 16:08:02 -0300

    Render carts for list products

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,9 +1,11 @@
 import { useSelector } from "react-redux"
+import { IState } from "../store"
+import { ICartItem } from "../store/modules/cart/types"
 
 
 
 export function Cart() {
-  const state = useSelector(state => state)
+  const cart = useSelector<IState, ICartItem[]>(state => state.cart.items)
 
   return (
     <table>
@@ -16,7 +18,14 @@ export function Cart() {
         </tr>
       </thead>
       <tbody>
-
+        {cart.map(item => (
+          <tr key={item.product.id}>
+            <td>{item.product.title}</td>
+            <td>{item.product.price}</td>
+            <td>{item.quantity}</td>
+            <td>{(item.product.price * item.quantity).toFixed(2)}</td>
+          </tr>
+        ))}
       </tbody>
     </table>
   )
